04488ef8353ebc21371e230cee571c46f4586090
dhanashravani <126771160+dhanashravani@users.noreply.github.com>
2023-03-03 12:33:11 +0530

    Update Employee.java

--- 02-HibernateCURD-Project/src/com/ram/hibernate/Employee.java
+++ 02-HibernateCURD-Project/src/com/ram/hibernate/Employee.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class Employee implements Serializable{
 private int empno;
 private String ename;
-private double sal;
+private double sal;//hgfds
 private Date hiredate;
 private int deptno;
 
